2df3d70ef3c9b9dcb8b7a012bc05f3af3506edfa
Lucas Oliveira <olivmath@protonmail.com>
2024-12-18 19:05:55 -0300

    update

--- src/Counter.sol
+++ src/Counter.sol
@@ -3,12 +3,31 @@ pragma solidity ^0.8.13;
 
 contract Counter {
     uint256 public number;
+    string public name;
+    uint8[] public array;
 
-    function setNumber(uint256 newNumber) public {
+    function setNamePublic(string memory newName) public {
+        name = newName;
+    }
+
+    function setNameExternal(string memory newName) external {
+        name = newName;
+    }
+
+    function setNumberPublic(uint256 newNumber) public {
         number = newNumber;
     }
 
-    function increment() public {
-        number++;
+    function setNumberExternal(uint256 newNumber) external {
+        number = newNumber;
+    }
+
+    function setArrayPublic(uint8[] memory newArray) public {
+       array = newArray;
+    }
+
+    function setArrayExternal(uint8[] memory newArray) external {
+       array = newArray;
+        
     }
 }
--- test/Counter.t.sol
+++ test/Counter.t.sol
@@ -9,16 +9,45 @@ contract CounterTest is Test {
 
     function setUp() public {
         counter = new Counter();
-        counter.setNumber(0);
     }
 
-    function test_Increment() public {
-        counter.increment();
-        assertEq(counter.number(), 1);
-    }
-
-    function testFuzz_SetNumber(uint256 x) public {
-        counter.setNumber(x);
+    /// @notice Testa o gás com `public`
+    function test_gasReportSetNumberPublic(uint256 x) public {
+        counter.setNumberPublic(x);
         assertEq(counter.number(), x);
+    }
+
+    /// @notice Testa o gás com `external`
+    function test_gasReportSetNumberExternal(uint256 x) public {
+        counter.setNumberExternal(x);
+        assertEq(counter.number(), x);
+    }
+
+    /// @notice Testa o gás com `public`
+    function test_gasReportSetNamePublic(string memory x) public {
+        counter.setNamePublic(x);
+        assertEq(counter.name(), x);
+    }
+
+    /// @notice Testa o gás com `external`
+    function test_gasReportSetNameExternal(string memory x) public {
+        counter.setNameExternal(x);
+        assertEq(counter.name(), x);
+    }
+
+    /// @notice Testa o gás com `public`
+    function test_gasReportSetArrayPublic(uint8[] memory x) public {
+        vm.assume(x.length > 0);
+        uint8 item = x[0];
+        counter.setArrayPublic(x);
+        assertEq(counter.array(0), item);
+    }
+
+    /// @notice Testa o gás com `external`
+    function test_gasReportSetArrayExternal(uint8[] memory x) public {
+        vm.assume(x.length > 0);
+        uint8 item = x[0];
+        counter.setArrayExternal(x);
+        assertEq(counter.array(0), item);
     }
 }
